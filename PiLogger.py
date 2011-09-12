fe00e41a496a122691101b3910085e76510f6611
Rajib Ahmed <cool_rajib@hotmail.com>
2011-09-12 19:09:46 +0200

    Added some comments to the seperate code bases

--- PiLogger.py
+++ PiLogger.py
@@ -1,3 +1,14 @@
+#!/usr/bin/python
+
+#############################################
+# This is script for reading webserver log
+# and generating report from that with bash
+#
+# @authors: Rajib Ahmed & Md. Abdur Razzak
+# @emails : {raae10,moca}@students.bth.se
+# @version: 1.0.0.dev
+#############################################
+
 import sys
 
 for args in sys.argv:
---
+++ bassLogger.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#############################################
+# This is script for reading webserver log
+# and generating report from that with bash
+#
+# @authors: Rajib Ahmed & Md. Abdur Razzak
+# @emails : {raae10,moca}@students.bth.se
+# @version: 1.0.0.dev
+#############################################
+echo
