7173062dd846c80df24cf23fd3720e97c1aba199
Rajib Ahmed <cool_rajib@hotmail.com>
2011-09-16 15:18:08 +0200

    Answered second question

--- bassLogger.sh
+++ bassLogger.sh
@@ -9,4 +9,11 @@
 # @version: 1.0.0.dev
 #############################################
 
+echo "Q1: Most number of connection attempts"
+echo "========================================"
 cat $1 | cut -d ' '  -f1 | sort | uniq -c | sort -rn | tee tmp.txt | head -n 10
+
+echo "\n\n"
+echo "Q2: Most number of successful connections"
+echo "========================================="
+cat $1 |  cut -d " " -f1,9 | grep 200$ | sort -rn | uniq -c | sort -rn | head -n 10
