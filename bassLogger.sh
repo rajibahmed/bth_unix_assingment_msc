83114d60953154ff42aa7aae75bbc7f34d917f77
Rajib Ahmed <cool_rajib@hotmail.com>
2011-09-19 00:24:45 +0200

    More fixes on argument passing

--- bassLogger.sh
+++ bassLogger.sh
@@ -2,7 +2,8 @@
 
 #############################################
 # This is script for reading webserver log
-# and generating report from that with bash
+# and generating report from that using
+# shell scripting
 #
 # @authors: Rajib Ahmed & Md. Abdur Razzak
 # @emails : {raae10,morc10}@students.bth.se
@@ -11,29 +12,29 @@
 
 #default variables
 VERSION="1.0.0.dev"
-n=20
+lim=20
 
 #set -- `getopt hn: $*`
 
 output_help(){
   echo "usage: options [n|h|f]"
+  exit ${1}
 }
 
 
 
 version(){
   echo "${0} is now at ${VERSION}"
+  exit 0
 }
 
 
 best_attempts(){
   echo "Q1: Most number of connection attempts   "
   echo "========================================="
-
   #conditional for time parsing
-  echo ${lim}
-  cat $file | cut -d ' '  -f1 | sort | uniq -c | sort -rn | head -n 10
-  #echo "\n\n"
+  cat $file | cut -d ' '  -f1 | sort | uniq -c | sort -rn | head -n ${lim}
+  echo "\n"
 }
 
 
@@ -43,8 +44,8 @@ successful_con(){
   echo "========================================="
 
   #conditional for time parsing
-  cat $file |  cut -d " " -f1,9 | grep 200$ | sort -rn | uniq -c | sort -rn | head -n 10 | sed "s/200$//"
-  echo "\n\n"
+  cat $file |  cut -d " " -f1,9 | grep 200$ | sort -rn | uniq -c | sort -rn | head -n ${lim} | sed "s/200$//"
+  echo "\n"
 }
 
 
@@ -54,30 +55,22 @@ successful_con(){
 # $@ is a special vaible that holds the
 # seperated
 if [ $# -lt 1 ]; then
-  output_help
-  exit 1
+  output_help 1
 fi
 
 while [ $# -gt 0 ]
 do
-  echo ${2}
   case ${1} in
-    -n | --number )   lim=${1}  ;;
-    -f | --file   )   file=${2} ;;
-    -v | --version)
-      version
-      exit 0
-      ;;
-    -h | --help   )
-      output_help
-      exit 0
-      ;;
-    -*)
-      echo $0: $1: unrecognized option >&2 ;;
+    -n | --number )   lim=${2}    ; shift  ;;
+    -f | --file   )   file=${2}   ; shift  ;;
+    -v | --version)   version     ;;
+    -h | --help   )   output_help 0 ;;
+    -*) echo $0: $1: unrecognized option >&2 ;;
      *) break   ;;
   esac
   shift
 done
 
+echo "\n"
 best_attempts
 successful_con
