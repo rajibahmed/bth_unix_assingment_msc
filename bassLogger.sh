5f2af44836ce415f02b6c19b47a3d68f839b5d7c
Rajib Ahmed <cool_rajib@hotmail.com>
2011-09-19 15:07:46 +0200

    added some improvements

--- bassLogger.sh
+++ bassLogger.sh
@@ -14,7 +14,6 @@
 VERSION="1.0.0.dev"
 lim=20
 
-#set -- `getopt hn: $*`
 
 output_help(){
   echo "usage: options [n|h|f]"
@@ -32,9 +31,9 @@ version(){
 best_attempts(){
   echo "Q1: Most number of connection attempts   "
   echo "========================================="
+
   #conditional for time parsing
   cat $file | cut -d ' '  -f1 | sort | uniq -c | sort -rn | head -n ${lim}
-  echo "\n"
 }
 
 
@@ -45,32 +44,35 @@ successful_con(){
 
   #conditional for time parsing
   cat $file |  cut -d " " -f1,9 | grep 200$ | sort -rn | uniq -c | sort -rn | head -n ${lim} | sed "s/200$//"
-  echo "\n"
 }
 
 
 
 
-# $@ is passed here
-# $@ is a special vaible that holds the
-# seperated
+# program exits when no
+# arguments are passed
 if [ $# -lt 1 ]; then
   output_help 1
 fi
 
+
+
+# This loop reads all the arguments passsed
+# trough the command line
 while [ $# -gt 0 ]
 do
   case ${1} in
     -n | --number )   lim=${2}    ; shift  ;;
     -f | --file   )   file=${2}   ; shift  ;;
-    -v | --version)   version     ;;
-    -h | --help   )   output_help 0 ;;
+    -v | --version)   version         ;;
+    -c )              best_attempts   ;;
+    -2 )              successful_con  ;;
+    -h | --help   )   output_help 0   ;;
     -*) echo $0: $1: unrecognized option >&2 ;;
      *) break   ;;
   esac
   shift
 done
 
+# Actual excution of functions to display data
 echo "\n"
-best_attempts
-successful_con
